a0e3282232dfb12386ef817e3be3794182863a10
Zsolt Molnár <zsotyooo@gmail.com>
2019-06-10 03:35:14 +0200

    add missing docs

---
+++ src/issue/IssueTag.preview.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "Issue Tag",
+  title: "Issue Tag",
+  context: {
+    key: 'PD-120',
+  },
+}
---
+++ src/text/TextWrapper.preview.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "Text Wrapper",
+  title: "Text Wrapper",
+  context: {
+    
+  },
+}
